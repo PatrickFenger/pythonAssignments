9459719066c457ca64c1996dbb10c7d8283026a4
Sean <seanaltoft@gmail.com>
2018-03-11 05:05:09 +0100

    add Assignment3.py readme

---
+++ Assignment_3/Assignment 3.py
@@ -0,0 +1,59 @@
+import webget
+import pandas as pd
+from glob import glob
+
+# Solutions to TEAM: Naughty Solution | Ali Khazendar, Casper Emde Christensen, Stephan Pedersen, Nicklas Vikke
+
+
+# Setting up usable csv file
+webget.download('https://raw.githubusercontent.com/INFINITE-KH/Python-Dataset/master/complete.csv')
+
+filed = glob('.\\complete.csv*')
+complete_csv = filed[0]
+
+data = pd.read_csv(complete_csv)
+
+#See labels for later use
+print(data.dtypes)
+
+
+##
+#Question 1: The 3 most expensive teams and the 3 cheapest teams according to player value.
+##
+
+df = data.groupby('club')['eur_value'].sum()
+print(df.sort_values(ascending=False))
+
+
+##
+#Question 2: Which nationality is the most frequent amongst all players
+##
+
+sf = data.groupby('nationality').count()
+
+print(sf.sort_values(['name'],ascending=False))
+
+##
+#Question 3: What is the difference between the release clause and the value of top 10 most valuable players
+##
+
+data['rel_value_diff'] = data['eur_release_clause'] - data['eur_value']
+asc_valued_players = data.iloc[:, [1, 16, 18, 185]].sort_values(['eur_value'], ascending=False).head(10)
+print(asc_valued_players)
+
+##
+#Question 4: What is the frequency of age, height and weight for all players
+##
+
+print(data.groupby(['age'])['age'].count())
+print(data.groupby(['height_cm'])['height_cm'].count())
+print(data.groupby(['weight_kg'])['weight_kg'].count())
+
+
+##
+#Question 5: How big is the average difference between value and wage of the players
+##
+
+data['val_wage_diff'] = data['eur_value'] - data['eur_wage']
+val_wage = data.iloc[:, [186]]
+print(val_wage.sum() % val_wage.count())
---
+++ Assignment_3/README.md
@@ -0,0 +1,33 @@
+## How to run file
+You run the file by entering python assignment_3.py with the file as argument so :
+
+assignment_3.py https://raw.githubusercontent.com/PatrickFenger/pythonAssignments/master/KoreanConflict.csv
+
+Its important that you have panda installed.
+
+When running the file the answers for question 1 and 4 will be showed in the terminal.
+The answer for the other questions an be found in the folder in form of plots.
+
+## Answers:
+### Q1: How many soldiers entered from a marine corps branch?
+
+4509  entered from the MARINE CORPS
+
+### Q2: Which enrollment was the most common?
+![alt text](https://github.com/PatrickFenger/pythonAssignments/blob/master/Assignment_2.2/q_2.png)
+
+We can see that the most common enrollment was Active-Regular
+
+### Q3: Was there an ethnicity majority throughout the war - If so, which?
+![alt text](https://github.com/PatrickFenger/pythonAssignments/blob/master/Assignment_2.2/q_3.png)
+
+We can see that there was a ethnicity majority , it was white.
+
+### Q4: Which division had the most casualties?
+
+The division with name: 38 INF 2 DIV
+
+### Q5: Which Home state suffered the most losses?
+![alt text](https://github.com/PatrickFenger/pythonAssignments/blob/master/Assignment_2.2/q_5.png)
+
+We can se that the state the suffered the most losses was Carlifonia 
