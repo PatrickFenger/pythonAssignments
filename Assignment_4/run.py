ff0e6d3cff7627767e9b7e0f174a5b51d3c78d46
Pravien <Pravien95@gmail.com>
2018-03-18 16:54:08 +0100

    tilføj graf

--- Assignment_4/run.py
+++ Assignment_4/run.py
@@ -7,6 +7,7 @@ import webget
 
 def question_1(file_path):
     start_year = {}
+    #Reads the csv file chuck by chuck - a chunk is 1 mb
     for chunk in pd.read_table(file_path, sep='\t', chunksize=1024):
         for elm in np.asarray(chunk)[0:, 5]:
             if not elm in start_year:
@@ -15,6 +16,7 @@ def question_1(file_path):
                 start_year[elm] += 1
     year = max(start_year , key=start_year .get)
     print("The year with most movies releases was: ",year)
+    # Plot the graph
     plt.bar(start_year .keys(), start_year .values(), width=0.3)
     plt.title('Amount of movies released pr year')
     plt.xticks(rotation=70, fontsize=5)
@@ -28,6 +30,7 @@ def question_1(file_path):
 
 def question_2(file_path):
     end_year = {}
+    # Reads the csv file chuck by chuck - a chunk is 1 mb
     for chunk in pd.read_table(file_path, sep='\t', chunksize=1024):
         for elm in np.asarray(chunk)[0:, 6]:
             if elm not in end_year:
@@ -37,6 +40,7 @@ def question_2(file_path):
         end_year.pop("\\N")
     year = max(end_year, key=end_year.get)
     print("The year where most series ended was: ", year)
+    #Plot the graph
     plt.bar(end_year.keys(), end_year.values(), width=0.3)
     plt.title('Amount of series that ended each year')
     plt.xticks(rotation=70, fontsize=5)
@@ -50,6 +54,7 @@ def question_2(file_path):
 
 def question_3(file_path):
     dict_genres = {}
+    # Reads the csv file chuck by chuck - a chunk is 1 mb
     for chunk in pd.read_table(file_path, sep='\t', chunksize=1024):
         for elm in np.asarray(chunk)[0:, 7:9]:
             genres = str(elm[1]).split(",")
@@ -63,7 +68,7 @@ def question_3(file_path):
                 else:
                     if dict_genres[genre] < runtime:
                         dict_genres[genre] = runtime
-
+    #Removes the key \\N
     dict_genres.pop("\\N")
     genre = max(dict_genres, key=dict_genres.get)
     print("The genre which has the movie/serie with the longest running time is ",genre)
@@ -71,6 +76,7 @@ def question_3(file_path):
 
 def question_4(file_path):
     genre_dict = {}
+    # Reads the csv file chuck by chuck - a chunk is 1 mb
     for chunk in pd.read_table(file_path, sep='\t', chunksize=1024):
         for elm in np.asarray(chunk)[0:,8]:
             genres = str(elm).split(",")
@@ -79,6 +85,7 @@ def question_4(file_path):
                     genre_dict[genre] = 1
                 else:
                     genre_dict[genre] += 1
+    # Removes the key \\N
     genre_dict.pop("\\N")
     genre = max(genre_dict, key=genre_dict .get)
     print("The genre the covers the most movies is: ",genre)
@@ -86,11 +93,12 @@ def question_4(file_path):
 
 def question_5(file_path):
     adult_runtime = {'runtime': 0, 'amount_of_movies': 0}
-    temp_list =[]
+    # Reads the csv file chuck by chuck - a chunk is 1 mb
     for chunk in pd.read_table(file_path, sep='\t', chunksize=1024):
         dd = chunk.as_matrix()
         mask = (dd[:, 4] == 1)
         for i in dd[mask][:,7]:
+            # only if the runtime is not \\N
             if i != '\\N':
                 adult_runtime['runtime'] += int(i)
                 adult_runtime['amount_of_movies'] += 1
@@ -107,6 +115,8 @@ def main(file_path):
 
 if __name__ in "__main__":
     try:
+        #Read the sys argeument at index 1
         main(sys.argv[1])
     except IndexError:
+        #If the argument can be found we download the file.
         main(webget.download("https://raw.githubusercontent.com/PatrickFenger/pythonAssignments/master/Assignment_4/title.basics.tsv"))
