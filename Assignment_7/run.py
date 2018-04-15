722c4cc701de7468ad93d78c47e229d64e6887ba
Martin Lodahl Andersen <jagthunde@hotmail.com>
2018-04-15 20:08:50 +0200

    Slow as fuck Q5

--- Assignment_7/run.py
+++ Assignment_7/run.py
@@ -15,21 +15,6 @@ ks_matrix = ks_df.as_matrix()
 
 # See columns for later use
 # print(ks_df.dtypes)
-def question_4():
-    mask = ((ks_matrix[:,9] == "successful") & (ks_matrix[:,13] > 5000.00))
-    main_success, success_count = np.unique(
-        ks_matrix[mask][:, 2], return_counts=True)
-    plt.figure("Question 4")
-    plt.title("number of successfully funded projects with more than 5.000$ pledged pr category")
-    plt.xlabel("Category")
-    plt.ylabel("successfully funded project")
-    plt.bar(main_success, success_count)
-    plt.show()
-
-def question_5():
-    under_10k = ((ks_matrix[:, 14] <= 10.000))
-
-
 def question_1():
     _, count = np.unique(ks_matrix[:,3], return_counts=True)
     mask = (ks_matrix[:,9] == "successful")
@@ -54,9 +39,9 @@ def question_2():
     grouping_ks_projects = ks_matrix[mask]
     category, count = np.unique(
         grouping_ks_projects[:, 2], return_counts=True)
-    category = category[np.argsort(-count)]  
+    category = category[np.argsort(-count)]
     count = np.sort(count)[::-1]
-   
+
     plt.figure("Question 2")
     plt.title("Proposed projects")
     plt.xlabel("Category")
@@ -76,8 +61,43 @@ def question_3():
     print("Question 3:")
     print(result)
 
+def question_4():
+    mask = ((ks_matrix[:,9] == "successful") & (ks_matrix[:,13] > 5000.00))
+    main_success, success_count = np.unique(
+        ks_matrix[mask][:, 2], return_counts=True)
+    plt.figure("Question 4")
+    plt.title("number of successfully funded projects with more than 5.000$ pledged pr category")
+    plt.xlabel("Category")
+    plt.ylabel("successfully funded project")
+    plt.bar(main_success, success_count)
+    plt.show()
 
-question_1()
-question_2()
-question_3()
-question_4()
+def question_5():
+    mask = (ks_matrix[:,9] == "successful")
+    main_success, main_count = np.unique(
+        ks_matrix[mask][:, 3], return_counts=True)
+    category = main_success[np.argsort(-main_count)[0]]
+    print("The category with most funded projects: " + category)
+
+    mask = (ks_matrix[:, 3] == category)
+    max_goal = ks_matrix[mask][:,14]
+    max_goal.sort()
+    max_goal = [i//1000 for i in max_goal]
+    most_succesfull_goal = (0,0)
+    for i in range(0, int(max_goal[-1])):
+        print(i)
+        numbers = [j for j in max_goal if j<i+10 and j>i]
+        sm = len(numbers)
+        if sm > most_succesfull_goal[1]:
+            most_succesfull_goal = (i, sm)
+
+    print("Question 5")
+    print("The range with most succesfull goal is : " + (str(most_succesfull_goal[0])) + " - "+(str(most_succesfull_goal[0]+10))+"k$")
+    print("With : "+(str(most_succesfull_goal[1])) +" succesfull projects")
+
+
+#question_1()
+#question_2()
+#question_3()
+#question_4()
+question_5()
